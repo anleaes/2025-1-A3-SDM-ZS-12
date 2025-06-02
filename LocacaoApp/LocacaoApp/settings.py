01fd0dd57351e896fb84e4d3153affddefdffd41
MPasin7 <matheus.pasin77@outlook.com>
2025-06-02 20:45:07 -0300

    att

--- LocacaoApp/LocacaoApp/settings.py
+++ LocacaoApp/LocacaoApp/settings.py
@@ -44,7 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'seguro.apps.SeguroConfig',
-    'CategoriaVeiculo.apps.CategoriaVeiculoConfig',
+    'CategoriaVeiculo.apps.CategoriaveiculoConfig',
 
 ]
 
