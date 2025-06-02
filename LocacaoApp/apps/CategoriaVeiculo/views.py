8557317e5fc7bb009f707c18e11e991e3bf3547d
MPasin7 <matheus.pasin77@outlook.com>
2025-06-02 20:33:36 -0300

    Views Criada

--- LocacaoApp/apps/CategoriaVeiculo/views.py
+++ LocacaoApp/apps/CategoriaVeiculo/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import CategoriaVeiculo
+from .serializer import CategoriaVeiculoSerializer
+from rest_framework import viewsets
 
-# Create your views here.
+class CategoriaVeiculoViewSet(viewsets.ModelViewSet):
+    queryset = CategoriaVeiculo.objects.all()
+    serializer_class = CategoriaVeiculoSerializer
