831a049e99a73bf575196740aed76ba504d9facf
MPasin7 <matheus.pasin77@outlook.com>
2025-06-02 16:35:27 -0300

    serializer att

---
+++ LocacaoApp/apps/seguro/serializer.py
@@ -0,0 +1,8 @@
+from .models import Seguro
+from rest_framework import serializers
+
+
+class EnderecoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Seguro
+        fields = '__all__'
