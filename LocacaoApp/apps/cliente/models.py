8649509e98eeec6508e4297b6867655946989739
MPasin7 <matheus.pasin77@outlook.com>
2025-06-02 20:51:53 -0300

    Models1

--- LocacaoApp/apps/cliente/models.py
+++ LocacaoApp/apps/cliente/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Cliente(models.Model):
+    nome = models.CharField(max_length=100)
+    cpf = models.CharField(max_length=14, unique=True)
+    cnh = models.CharField(max_length=20, unique=True)
+    telefone = models.CharField(max_length=15)
+    email = models.EmailField()
+    data_nascimento = models.DateField()
+    endereco = models.CharField(max_length=255)
+    cidade = models.CharField(max_length=100)
+
+    class Meta:
+        verbose_name = 'Cliente' 
+        verbose_name_plural = 'Clientes'
+        ordering = ['nome']
+
+    def __str__(self):
+        return self.nome
