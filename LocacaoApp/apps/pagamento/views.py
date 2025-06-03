0a6a83ffc449af7359d787323fe1f7fe7a3ab002
Matheus Pasin <matheus.pasin77@outlook.com.br>
2025-06-03 09:51:58 -0300

    pagamento criado

--- LocacaoApp/apps/pagamento/views.py
+++ LocacaoApp/apps/pagamento/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Pagamento
+from .serializer import PagamentoSerializer
+from rest_framework import viewsets
 
-# Create your views here.
+class LocacaoViewSet(viewsets.ModelViewSet):
+    queryset = Pagamento.objects.all()
+    serializer_class = PagamentoSerializer
