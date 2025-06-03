5701b1db88c6210b31dd7a857498972b74aceb72
MPasin7 <matheus.pasin77@outlook.com>
2025-06-02 23:12:45 -0300

    views criada

--- LocacaoApp/apps/veiculo/views.py
+++ LocacaoApp/apps/veiculo/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Veiculo
+from .serializer import VeiculoSerializer
+from rest_framework import viewsets
 
-# Create your views here.
+class VeiculoViewSet(viewsets.ModelViewSet):
+    queryset = Veiculo.objects.all()
+    serializer_class = VeiculoSerializer
