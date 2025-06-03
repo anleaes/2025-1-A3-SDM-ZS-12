22aed901b9515f4afe7c9093f64fcec4147564fe
MPasin7 <matheus.pasin77@outlook.com>
2025-06-02 23:27:41 -0300

    Views Pronta

--- LocacaoApp/apps/locacao/views.py
+++ LocacaoApp/apps/locacao/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Locacao
+from .serializer import LocacaoSerializer
+from rest_framework import viewsets
 
-# Create your views here.
+class ClienteViewSet(viewsets.ModelViewSet):
+    queryset = Locacao.objects.all()
+    serializer_class = LocacaoSerializer
